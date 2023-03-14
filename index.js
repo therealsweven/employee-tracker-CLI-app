40d6a51db3b569bb600bf009799bed0c3b158550
therealsweven <robssimpson11@gmail.com>
2023-03-14 17:47:25 -0600

    Added main menu

---
+++ index.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql2");
+const CLI = require("./CLI");
+const cli = new CLI();
+cli.run();
+
+const db = mysql.createConnection(
+  {
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+db.query("SELECT * FROM department", function (err, results) {
+  //   console.log(results);
+  return results;
+});
+
+console.log(deptdata);
+
+// db.query("SELECT * FROM role", function (err, results) {
+//   //   console.log(results);
+// });
+// db.query("SELECT * FROM employee", function (err, results) {
+//   //   console.log(results);
+// });
