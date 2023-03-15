de7f3f4c13672969956f2ba8e0a6bec66e9d0dfa
therealsweven <robssimpson11@gmail.com>
2023-03-14 20:43:37 -0600

    Can now add employees and roles

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 const mysql = require("mysql2");
 const CLI = require("./lib/CLI");
 const cli = new CLI();
+
 cli.run();
-
-// const db = mysql.createConnection(
-//   {
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "root",
-//     database: "employees_db",
-//   },
-//   console.log(`Connected to the employees_db database.`)
-// );
-
-// db.query("SELECT * FROM department", function (err, results) {
-//   //   console.log(results);
-//   return results;
-// });
